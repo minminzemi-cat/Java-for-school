9d65c4fbf46b7cc71180cea291c666a690bbd8a3
oomori <2511005@i-seifu.jp>
2026-06-03 10:10:59 +0900

    printfの練習ソースコード

---
+++ 0603/Printf.java
@@ -0,0 +1,12 @@
+public class Printf {
+    public static void main(String[] args){
+        //C
+        // #include<stdio.h>
+        //int value=100;
+        //printf("%d",value);
+        int value= 100;
+        double value2=3.14;
+        System.out.printf("%d   %f",value,value2);
+        
+    }
+}
